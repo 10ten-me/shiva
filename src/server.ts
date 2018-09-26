91b38e64bfa80cb609b8d3d3085ffa9cd8a385d2
Loic Tosser <loic@10ten.ae>
2018-09-26 15:38:27 +0400

    refactor

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,12 @@
 import app from './app';
 import { Config } from './config';
-import * as socketIo from 'socket.io-client';
 import { ZiwoWs } from './ziwo';
 
-var config = new Config();
+const config = new Config();
+const ziwoWs = new ZiwoWs(config);
 
 app.listen(config.port, () => {
     console.log('Express server listening on port ' + config.port);
 });
 
-const socket = socketIo(config.ziwoApiUrl, {
-    path: '/socket',
-    query: 'access_token=' + config.ziwoAdminToken,
-    reconnection: true
-});
-
-socket.on('GET /live/queues', ZiwoWs.queues());
-
-socket.on('GET /live/numbers', function (data) {
-    console.log(data);
-});
-
-socket.on('GET /live/positions', function (data) {
-    console.log(data);
-});
-
-socket.on('GET /live/agents', function (data) {
-    console.log(data);
-});
-
-socket.on('GET /live/calls', function (data) {
-    console.log(data);
-});
-
-socket.on('GET /live/queueCalls', function (data) {
-    console.log(data);
-});
+ziwoWs.listen();
--- src/ziwo.ts
+++ src/ziwo.ts
@@ -1,21 +1,46 @@
-export class ZiwoWs {
+import * as socketIo from 'socket.io-client';
+import { Config } from './config';
 
-  function queues(data: any) {
-    console.log(data)
+
+export class ZiwoWs {
+  public socket: socketIo;
+
+  constructor(config: Config) {
+    this.socket = socketIo(config.ziwoApiUrl, {
+      path: '/socket',
+      query: 'access_token=' + config.ziwoAdminToken,
+      reconnection: true
+    });
   }
-  function numbers(data: any) {
-    console.log(data)
+
+  public listen() {
+    this.socket.on('GET /live/queues', this.queues);
+    this.socket.on('GET /live/numbers', this.numbers);
+    this.socket.on('GET /live/positions', this.positions);
+    this.socket.on('GET /live/agents', this.agents);
+    this.socket.on('GET /live/calls', this.calls);
+    this.socket.on('GET /live/queueCalls', this.queueCalls);
+    this.socket.on('GET /live/stats', this.stats);
   }
-  function positions(data: any) {
-    console.log(data)
+  public queues(data: any) {
+    console.log(data);
   }
-  function agents(data: any) {
-    console.log(data)
+  public numbers(data: any) {
+    console.log(data);
   }
-  function calls(data: any) {
-    console.log(data)
+  public positions(data: any) {
+    console.log(data);
   }
-  function queueCalls(data: any) {
-    console.log(data)
+  public agents(data: any) {
+    console.log(data);
+  }
+  public calls(data: any) {
+    console.log(data);
+  }
+  public queueCalls(data: any) {
+    console.log(data);
+  }
+  public stats(data: any) {
+    console.log(data);
   }
 }
